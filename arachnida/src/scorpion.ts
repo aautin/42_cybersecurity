85c3d3be9d277d0204bf28492f9a7c2099a29900
aautin.home <alexandreautin.aa@gmail.com>
2025-06-06 14:20:32 +0200

    parse the files in the right order (left to right)

--- arachnida/src/scorpion.ts
+++ arachnida/src/scorpion.ts
@@ -1,7 +1,6 @@
 import * as fs from  'fs/promises';
 import exifr from 'exifr';
 
-
 async function scorpion() {
 	let filenames = process.argv.slice(2);
 
@@ -11,19 +10,21 @@ async function scorpion() {
 	}
 
 	while (filenames.length) {
-		let file: string = filenames.pop()!;
+		let file: string = filenames.shift()!;
 
 		try {
 			// Check file stats
 			const stats = await fs.stat(file);
+			console.log(`-----${file}-----`);
 			console.log(stats);
 
 			// Optionally filter JPEG files by extension
 			if (/\.jpe?g$/i.test(file)) {
 				const exifr_content = await exifr.parse(file);
 				if (exifr_content)
-					console.log('EXIF data:', exifr_content);
+					console.log('EXIF', exifr_content);
 			}
+			console.log(``);
 		} catch (error: any) {
 			console.error(`\x1b[31m${error.message}\x1b[0m`);
 		}
