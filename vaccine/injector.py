fb7fd862d17aa576c2f37b22bebe21e16922b9b8
aautin <alexandreautin.aa@gmail.com>
2025-08-24 21:07:24 +0200

    trying to define the injections potential points

--- vaccine/injector.py
+++ vaccine/injector.py
@@ -14,15 +14,41 @@ def parse_args():
 
 	return args.x.upper(), args.url, dict(args.header) if args.header else {}
 
+def parse_headers(headers: dict) -> list[dict]:
+    result = []
+    for key, value in headers.items():
+        pairs = []
+        for item in value.split(';'):
+            item = item.strip()
+            if '=' in item:
+                k, v = item.split('=', 1)
+                pairs.append({k: v})
+        result.append({key: pairs})
+    return result
+
+def get_query_params(url : str) -> list[str]:
+	if '?' in url:
+		query_list = url.split('?', 1)[1].split('&')
+		query_list = [param for param in query_list if '=' in param]
+		return [param.split('=', 1)[0] for param in query_list]
+	return []
+
+def inject_payload(url: str, method: str = "GET", headers: dict = {}, payload: str = "' OR '1'='1") -> requests.Response:
+	query_params = get_query_params(url)
+	if not query_params:
+		print("[!] No query parameters found in the URL.")
+		return None
+
+	response = requests.request(method, url.split('?', 1)[0], headers=headers)
+
+	return response
+
 def main():
 	method, url, headers = parse_args()
 
 	try:
-		print(f"[*] Sending {method} request to {url}, with headers: {headers}")
-		response = requests.request(method, url, headers=headers)
-
-		print(f"[*] Response code: {response.status_code}")
-		print(f"[*] Response body:\n{response.text}")
+		print(f"[*] Sending {method} request to {url}, with headers {headers} and queryies {get_query_params(url)}")
+		inject_payload(url, method, headers)
 	except:
 		print("[!] An error occurred while making the request.")
 
