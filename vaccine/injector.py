b89fca52ff85dcb1e4c081d5a75da4b67ee0aa4c
aautin <alexandreautin.aa@gmail.com>
2025-08-27 14:01:19 +0200

    add parsing to reassemble params, start building records

--- vaccine/injector.py
+++ vaccine/injector.py
@@ -19,32 +19,51 @@ def parse_args():
 	body_data = args.body if args.body else ""
 	return args.X.upper(), args.url, dict(args.header) if args.header else {}, body_data
 
-def get_query_params(url : str) -> dict[str, str]:
-	if '?' in url:
-		query_list = url.split('?', 1)[1].split('&')
-		query_list = [param for param in query_list if '=' in param]
-		return {param.split('=', 1)[0]: param.split('=', 1)[1] for param in query_list}
+
+
+def body_params(body_string: str) -> dict[str, str]:
+	if body_string:
+		body_list = body_string.split('&')
+		return {param.split('=', 1)[0]: param.split('=', 1)[1] if '=' in param else '' for param in body_list}
 	return {}
 
-def get_body_params(body_data: str) -> dict[str, str]:
-	if body_data:
-		body_list = body_data.split('&')
-		return {param.split('=', 1)[0]: param.split('=', 1)[1] for param in body_list if '=' in param}
-	return {}
+def body_string(body_params: dict[str, str]) -> str:
+	return '&'.join([f"{key}={value}" for key, value in body_params.items()])
+
+
+
+def query_params(query_string : str) -> dict[str, str]:
+	if not query_string: return {}
+	query_list = query_string.split('&')
+	return {param.split('=', 1)[0]: param.split('=', 1)[1] if '=' in param else '' for param in query_list}
+
+def query_string(query_params: dict[str, str]) -> str:
+	return '&'.join([f"{key}={value}" for key, value in query_params.items()])
+
+def extract_query_string(full_url: str) -> str:
+	return full_url.split('?', 1)[1] if '?' in full_url else ''
+
+
+
+def assemble_full_url(base_url: str, query_string: str) -> str:
+	return base_url + ('?' + query_string if query_string else '')
+
+def extract_base_url(full_url: str) -> str:
+	return full_url.split('?', 1)[0]
 # ----------------------- #
 
 
-def get_default_request_record(method: str, url: str, headers: dict, body: dict) -> dict:
-	request = requests.request(method, url, headers=headers, data=body if method == "POST" else None)
+def default_request_record(method: str, full_url: str, headers: dict, body_string: str) -> dict:
+	request = requests.request(method, full_url, headers=headers, data=body_string if method == "POST" else None)
 	time = request.elapsed.total_seconds()
 	for _ in range(10):
-		req = requests.request(method, url, headers=headers, data=body if method == "POST" else None)
+		req = requests.request(method, full_url, headers=headers, data=body_string if method == "POST" else None)
 		if req.elapsed.total_seconds() > time:
 			time = req.elapsed.total_seconds()
 
 	return {
-		"body": get_body_params(body) if method == "POST" else {},
-		"query": get_query_params(url),
+		"body": body_params(body_string),
+		"query": query_params(extract_query_string(full_url)),
 		"response": {
 			"status_code": request.status_code,
 			"content": request.content.decode(),
@@ -52,15 +71,31 @@ def get_default_request_record(method: str, url: str, headers: dict, body: dict)
 		}
 	}
 
+def detection_request_records(method: str, full_url: str, headers: dict, body_string: str) -> list[dict]:
+	body_params = body_params(body_string)
+	query_params = query_params(extract_query_string(full_url))
+	params = body_params + query_params
+
+	detection_request_records = []
+	
+	# To be continued...
+	# Modify one different parameter for each request with "INJECTED" value
+	# Reassemble body and query strings then request and store the response as a record in detection_request_records
+	return detection_request_records
+
 def main():
 	method, url, headers, body = parse_args()
 
 	try:
-		default_record = get_default_request_record(method, url, headers, body)
-		print(f"[*] Default request record: {default_record}")
+		records = {
+			"default": default_request_record(method, url, headers, body),
+			"detections": detection_request_records(method, url, headers, body)
+		}
+		# To be continued...
+		# Print the records body and query parameters
 
 	except:
-		print("[!] An error occurred while making the request.")
+		print("[!] An error occurred while making the requests.")
 
 if __name__ == "__main__":
 	main()
